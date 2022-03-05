e7c69ff08023f75f13c2f96684fc82a3d09bfe54
Alexey Belyaev <bel.alexeyv@gmail.com>
2022-03-05 14:06:00 +0300

    plchat change 1

--- src/com/alexeybelyaev/Main.java
+++ src/com/alexeybelyaev/Main.java
@@ -3,6 +3,7 @@ package com.alexeybelyaev;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Plchat1 plchat1 = new Plchat1();
+        plchat1.print();
     }
 }
