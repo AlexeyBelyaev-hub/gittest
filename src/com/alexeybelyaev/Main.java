b46c168ef32bb8ccb3a59b958d56a5d5320ec680
Alexey Belyaev <bel.alexeyv@gmail.com>
2022-03-05 14:35:06 +0300

    Plchat squashed

--- src/com/alexeybelyaev/Main.java
+++ src/com/alexeybelyaev/Main.java
@@ -3,6 +3,10 @@ package com.alexeybelyaev;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Plchat1 plchat1 = new Plchat1();
+        plchat1.print(); // change 1
+        plchat1.print(); // change 2
+        plchat1.print(); // change 3
+        plchat1.print(); // change 4
     }
 }
---
+++ src/com/alexeybelyaev/Plchat1.java
@@ -0,0 +1,7 @@
+package com.alexeybelyaev;
+
+public class Plchat1 {
+    public void print(){
+        System.out.println("PLCHAT done");
+    }
+}
